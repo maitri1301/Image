94909c269b6fbb36ea5c125eeae7a28256195438
Maitri Prajapti <maitri@zealousweb.com>
2024-03-15 11:20:34 +0530

    remove commented code

--- wp-content/plugins/upload/build/index.asset.php
+++ wp-content/plugins/upload/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7e94858c48b7126cb87d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dcd8ec2f9b4885d056b4');
